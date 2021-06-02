42a94e8f0304c63fa2fbe578dd3464cdae415b5c
Davi Marques <davimgcb@gmail.com>
2021-06-02 01:21:42 -0300

    iniciando e adicionado as tags

---
+++ frontweb/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h2>AGORA VAI MOISÉS </h2>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ frontweb/utils/tags.js
@@ -0,0 +1,70 @@
+//compile CONTROL + ALT + N
+
+const tags = 
+    {
+        bussines: [
+            'business', 'empreendorismo' , 'marketing', 'sucesso', 'marketing digital', 'loja fisica', 'negocios online',
+            'pequeno negocio', 'mulheres de negocio', 'pequena empresa', 'grande empresa', 'negocios inteligentes',
+            'instagram para negocios', 'negocios imobiliarios', 'gestao de negocios', 'agronegocios', 'negocios locais',
+            'negocios internacionais', 'negocios sustentaveis', 'desenvolvendo pessoas', 'coach de negocios', 'desenvolvimento de negocios',
+            'negócios pet', 'midias sociais para negocios', 'negócios milionarios', 'midias digitais para negocios', 'negócio em casa' , 'micronegocios',
+            'empreender','motivacao','lideranca','dinheiro','foco','carreira','crescimento','vendas','networking','altaperformance',
+            'mindset', 'lifestyle','oportunidade', 'empreendedorismo transforma', 'economia', 'trader', 'formado', 'bolsa de valores',
+            'investimentos', 'cripto moeda', 'bit coin', 'diplomata'
+        ],
+        photograph: [
+            'casamento','festa','formatura','eventos','praia','modelo','natureza','viagem','canon',
+            'comida','bebes','gravidez','bandas','cantor','cerimonias','nikon','sony','4k','HD','Full HD','variedades de lentes',
+            'iluminacao', 'videoclipe', 'clipe musical', 'indie', 'artistica', 'conceitual', 'neon', 'zoom', 'filtro',
+            'tratamento', 'edicao', 'design', 'curso', 'conteudo adulto', 'nudismo','formado','pinterest','casal','namoro',
+            'documentacao', 'album', 'book', 'poster', 'quadro', 'camisas','moletom','canecas','cadernos', 'almofadas'
+        ],
+        video: [
+            'youtuber', 'criador de conteudo', 'canal pequeno', 'canal grande', 'roteirista', 'editor', 'producao',
+            'marketing','vinheta','abertura', 'cg', 'conteudo infantil', 'cristao', 'musica',
+            'conteudo de jogos', 'conteudo de maquiagem', 'vlog', 'daily vlog', 'podcast', 'adulto','network', 'produtora',
+            'tv', 'tv aberta', 'tv fechada', 'propaganda', 'politica', 'economia', 'opiniao', 'react', 'tik tok', 'instagram',
+            'kwai', 'esportes', 'animes', 'gamer', 'comedia', 'terror', 'jumpscare', 'analises', 'review', 'cinema',
+            'documentario', 'tutorial', 'do yourself', 'trends', 'desafios', 'violento', 'policial', 'noticias'
+        ],
+        music: [
+            'autoral', 'cover', 'musico', 'produtor', 'tecnico de som', 'mixagem', 'remasterizacao', 'empresario',
+            'casa de eventos', 'pub', 'musico de bar', 'musico de estudio', 'banda', 'cordas', 'metais', 'percussao',
+            'voz', 'vocalista', 'backing vocal', 'orquestra', 'musica classica', 'dj', 'mc', 
+            'gospel', 'sertanejo','funk', 'trap', 'rock', 'metal', 'bossa nova', 'axe', 'anos 60', 'anos 70', 'anos 80', 'anos 90',
+            'indie', 'pop', 'nacional', 'internacional', 'rap', 'hip hop', 'jazz', 'samba', 'pagode', 'black music', 'blues',
+            'country', 'emocore', 'folk', 'grunge', 'lo-fi','new age','mpb', 'punk', 'r&b', 'reggae', 'infantil', 'trap', 'soul music',
+            'surf music'
+        ],
+        education:[
+            'professor', 'instrutor', 'escola','ensino fundamental', 'ensino medio' ,'faculdade', 'pos graduacao', 'mestrado', 'pos doutorado',
+            'exatas', 'humanas', 'biologicas', 'tecnologia', 'historia', 'geografia', 'engenharia', 'letras',
+            'portugues','matematica', 'ciencias', 'educacao fisica', 'fisica','artes', 'quimica',
+            'ingles', 'espanhol', 'frances','alemao','mandarim', 'italiano', 'linguas extrangeiras' , 
+            'area da saude', 'educacao musical', 'educacao financeira', 'etiqueta', ''
+        ],
+        food:[
+
+        ],
+        trip:[
+
+        ],
+        events:[
+
+        ],
+        health:[
+
+        ],
+        fashion:[
+
+        ],
+        artist:[
+
+        ],
+        geek:[
+
+        ]
+    }
+
+
+console.log(tags.education.length)
